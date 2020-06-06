6c8436d791b21134c36268367da38e11522d62ba
Cyril de Vaucleroy <cyril.devaucleroy@gmail.com>
2020-06-06 08:35:37 +0200

    Terrible script because I'm too lazy to use stow

---
+++ too_lazy_to_use_stow.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+LIST_FILES="
+  /usr/local/bin/upgrade-kernel.sh
+  /usr/local/bin/update-gentoo.sh
+  ~/.config/herbstluftwm/
+  /etc/portage/
+  /usr/local/portage/profiles/
+"
+
+for f in $LIST_FILES ; do
+  # hardcoded exceptions
+  if [ "$f" = "/usr/local/portage/profiles/" ] ; then
+    cp -r "$f" custom_profiles
+  else
+    cp -r "$f" .
+  fi
+done
+
